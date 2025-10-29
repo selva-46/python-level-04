c96e1886412d65762929bb3ba7c009aae5cdbc91
Selva M <selvapro94@gmail.com>
2025-10-29 03:31:33 -0700

    Create ex-18.py

---
+++ ex-18.py
@@ -0,0 +1,11 @@
+print("Fibonnaci Series")
+print("----------------")
+n=int(input("Enter the Number"))
+print("Fibonnaci Series")
+a=n-1
+b=1
+for i in range(n+1):
+   c=a+b
+   print(c)
+   a+b
+   b=c
